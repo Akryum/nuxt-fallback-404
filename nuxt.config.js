573b5e3c7d5a499a29f9328e841b98571def8418
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-07-13 18:15:31 +0200

    fix: router 404 page

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,5 +70,14 @@ export default {
     config: {
       devtools: true
     }
+  },
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: "custom",
+        path: "*",
+        component: resolve(__dirname, "pages/404.vue")
+      });
+    }
   }
 };
