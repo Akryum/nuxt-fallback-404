7b5cdce02e5d928f400ab64ec49f8116514fa013
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-07-13 17:59:15 +0200

    init

---
+++ pages/404.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data () {
+    return {
+      planet: null
+    }
+  },
+
+  async fetch () {
+    this.planet = await this.$axios.$get('https://swapi.dev/api/planets/1/')
+  }
+}
+</script>
+
+<template>
+  <div class="planet">
+    <h1>Not found</h1>
+    <h2>{{ planet.name }}</h2>
+    <pre>{{ planet }}</pre>
+  </div>
+</template>
